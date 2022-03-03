7bbe102f85bc7f7b65bc0b0be59fcd6a16fab90b
GiovanniCalia <johnca@outlook.it>
2022-03-03 16:45:29 +0100

    Comprensione dell'esercizio tramite console.log

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const eleLevel = document.getElementById("level");
 const btnPlay = document.getElementById("play");
 const eleGrid = document.querySelector(".grid");
@@ -8,19 +7,23 @@ btnPlay.addEventListener("click", setupGame);
 
 arrLevels = [100, 81, 49];
 
-
 function setupGame(){
-    eleGrid.innerHtml = "";
+    eleGrid.innerHTML = "";
     const indexLevel = parseInt(eleLevel.value);
+    console.log(indexLevel);
     const cellsCount = arrLevels[indexLevel];
+    console.log(cellsCount);
     const cellsPerRow = Math.sqrt(cellsCount);
+    console.log(cellsPerRow);
 
     for (let cellNum = 1; cellNum <= cellsCount; cellNum++){
         const eleCell = document.createElement("div");
         eleCell.classList.add("cell");
+        console.log(eleCell);
         eleCell.innerHTML = cellNum; 
         eleCell.style.width = `calc(100% / ${cellsPerRow})`;
         eleCell.style.height = `calc(100% / ${cellsPerRow})`;
         eleGrid.append(eleCell);
+        console.log(eleGrid)
     }
 }
