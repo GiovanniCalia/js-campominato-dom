9e5a9ce395b4557340bcb30f66d56257861cd957
GiovanniCalia <johnca@outlook.it>
2022-03-04 15:45:04 +0100

    Completamento esercizio (copiato ma capito)

--- js/script.js
+++ js/script.js
@@ -1,43 +1,74 @@
 const eleLevel = document.getElementById("level");
 const btnPlay = document.getElementById("play");
 const eleGrid = document.querySelector(".grid");
-
+const eleOutput =  document.querySelector(".output");
+arrLevels = [100, 81, 49]; 
+const numberBomb = 16;
 
 btnPlay.addEventListener("click", setupGame);
-
-arrLevels = [100, 81, 49];
+eleLevel.addEventListener("change", setupGame);
 
 function setupGame(){
     eleGrid.innerHTML = "";
+    eleOutput.innerHTML = "";
+    score = 0;
     const indexLevel = parseInt(eleLevel.value);
-    //console.log(indexLevel);
     const cellsCount = arrLevels[indexLevel];
-    //console.log(cellsCount);
     const cellsPerRow = Math.sqrt(cellsCount);
-    //console.log(cellsPerRow);
-    //let bombNumber = [];
+    const goodCells = cellsCount - numberBomb;
 
 
     for (let cellNum = 1; cellNum <= cellsCount; cellNum++){
         const eleCell = document.createElement("div");
         eleCell.classList.add("cell");
-        //console.log(eleCell);
         eleCell.innerHTML = cellNum; 
         eleCell.style.width = `calc(100% / ${cellsPerRow})`;
         eleCell.style.height = `calc(100% / ${cellsPerRow})`;
         eleGrid.append(eleCell);
-        eleCell.addEventListener("click", () => eleCell.classList.add("red"));
-        //console.log(eleGrid); 
+        eleCell.addEventListener("click", manageCellClick);
     }
-    for (let casNum = 1; casNum <= 16; casNum++){
-        let casualsNum = Math.floor(Math.random() * (100 - 1 + 1) ) + 1;
-        let bombNumber = [];
-        bombNumber.push(casualsNum);
-        while (bombNumber.includes(casualsNum)){
-            casualsNum = Math.floor(Math.random() * (100 - 1 + 1) ) + 1
-        }
-        console.log(bombNumber);
-    }  
 
+    const arrRandom = [];
+    for (let i = 1; i <= numberBomb; i++){
+        let randomNumber;
+    do {
+        randomNumber = getRandomnumber(1, cellsCount);
+    } while (arrRandom.includes(randomNumber)){
+        (arrRandom.push(randomNumber))
+    }
+    console.log(arrRandom.sort());
+    } 
+
+    function manageCellClick(){
+        cellValue = parseInt(this.innerHTML);
+        console.log("valore cella", cellValue);
+        
+            if (arrRandom.includes(cellValue)){
+                this.classList.add("redBomb");
+                eleOutput.innerHTML = `Hai preso una bomba. Il tuo punteggio è:` + score; // oppure fai document.querySelectorAll(".blue").length;, al posto di score.
+                const cells = document.querySelectorAll(".cell");
+                for (i = 0; i < cells.length; i++){
+                    cells[i].removeEventListener("click", manageCellClick);
+                }
+            } else {
+                this.classList.add("blue");
+                score++;
+            }
+        
+        if (score == goodCells ){
+            eleOutput.innerHTML = `Hai vinto! Il tuo punteggio è:` + score;
+            const cells = document.querySelectorAll(".cell");
+            for (i = 0; i < cells.length; i++){
+                cells[i].removeEventListener("click", manageCellClick);
+            }
+        }
+
+        this.removeEventListener("click", manageCellClick);
+    }
 }
+
+function getRandomnumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 
